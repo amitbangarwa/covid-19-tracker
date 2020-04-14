2e3dc7e047bae0e301fab638efa35fe177df89f4
Amit B <amit.bangarwa@healthcareinnovate.com.au>
2020-04-14 14:27:17 +0530

    Updates

---
+++ precache-manifest.12458c2d3f8e5168ad33e8ca02bc04e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ae540f42bf1d629548f32768a5fed1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7431c767b25b19b64e08",
+    "url": "/static/css/main.c2440946.chunk.css"
+  },
+  {
+    "revision": "8a5829b2adf615c32f74",
+    "url": "/static/js/2.73df5604.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.73df5604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7431c767b25b19b64e08",
+    "url": "/static/js/main.0a967235.chunk.js"
+  },
+  {
+    "revision": "d9e25c221f8d82a16828",
+    "url": "/static/js/runtime-main.85a8ccee.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
